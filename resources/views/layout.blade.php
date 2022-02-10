b2914025c2c560f94c1eae443fa728dfb85283e5
Marhaensalenindo Komara <marhaensa@gmail.com>
2022-02-10 19:33:39 +0700

    fixing view and add some color css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -77,7 +77,7 @@
 
     <script src="{{ asset('assets/bundles/mainscripts.bundle.js') }}"></script>
     <script type="text/javascript" src="cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="jquery-3.5.1.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{ asset('assets/js/index.js') }}"></script>
     @yield('script')
