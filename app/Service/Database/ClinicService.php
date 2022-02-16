c243edff6b46164e96c865cdf9d4a4f1453438fd
Marhaensalenindo Komara <marhaensa@gmail.com>
2022-02-16 10:06:17 +0700

    create service clinic

--- app/Service/Database/ClinicService.php
+++ app/Service/Database/ClinicService.php
@@ -3,6 +3,8 @@
 namespace App\Service\Database;
 
 use App\Models\Clinic;
+use Illuminate\Contracts\Validation\Rule;
+use Ramsey\Uuid\Uuid;
 
 class ClinicService {
 
@@ -23,6 +25,37 @@ class ClinicService {
         $clinic = Clinic::findOrfail($clinicId);
 
         return $clinic->toArray();
+    }
+
+    public function create($payload)
+    {
+        $clinic = new Clinic();
+        $clinic->id = Uuid::uuid4()->toString();
+        $clinic = $this->fill($clinic, $payload);
+        $clinic->save();
+
+        return $clinic;
+    }
+
+    private function fill(Clinic $clinic, array $attributes)
+    {
+
+        foreach ($attributes as $key => $value) {
+            $clinic->$key = $value;
+        }
+
+        Validator::make($clinic->toArray(), [
+            'name' => 'required|string',
+            'address' => 'required|string',
+            'about' => 'required|string',
+            'facility' => 'required',
+            'service' => 'required',
+            'contact' => 'nullable|numeric',
+            'email' => 'nullable|email',
+            'profile_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ])->validate();
+
+        return $clinic;
     }
 
 }
