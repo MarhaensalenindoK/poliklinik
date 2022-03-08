d9db0f8b9298f7d4e06bad5d758b104126d72063
Marhaensalenindo Komara <marhaensa@gmail.com>
2022-03-08 09:26:57 +0700

    fix color button

--- resources/views/admin/modals/_create_account.blade.php
+++ resources/views/admin/modals/_create_account.blade.php
@@ -42,7 +42,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-round btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-round btn-danger" onclick="createAccount()">Confirm</button>
+                <button type="button" class="btn btn-round btn-primary" onclick="createAccount()">Confirm</button>
             </div>
         </div>
     </div>
